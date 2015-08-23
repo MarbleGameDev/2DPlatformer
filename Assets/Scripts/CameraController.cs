88cc807526e85b2c9c22a56d41d49cb7845d06d4
thejereman13 <jereman13@gmail.com>
2015-08-23 15:24:13 -0400

    Player Movement and Prelim Scene setup
    main character can move up and to the sides, and the camera follows him.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public GameObject player;
+	private int cameraOffset = -10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, cameraOffset);
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public float movementModifier;
+	public float verticalModifier;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputUp = Input.GetAxis("Vertical");
+		float inputSide = Input.GetAxis ("Horizontal");
+		
+		if (inputSide > 0){
+			transform.Translate(Vector2.right * movementModifier);
+		}
+		if (inputSide < 0){
+			transform.Translate(Vector2.left * movementModifier);
+		}
+		if (inputUp > 0) {
+			transform.Translate(Vector2.up * verticalModifier);
+		}
+	}
+}
