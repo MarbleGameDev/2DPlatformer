32dda0f0b16fbb2f699e65b4a5f7b44dd81089ad
Uberpangolin <jolewis@student.csdspartans.org>
2015-08-25 18:31:25 -0400

    Fixed dumb double jump
    TO DO: create a parabolic funtion that the character moves on throuhg
    the jump that is effected by velocity through the air.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,17 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 	public float movementModifier;
 	public float verticalModifier;
+	public bool canJump;
+	//public float test = 1.0f;
 	// Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		//test = test + 0.1;
 		float inputUp = Input.GetAxis("Vertical");
 		float inputSide = Input.GetAxis ("Horizontal");
 		
@@ -20,8 +24,19 @@ public class PlayerMovement : MonoBehaviour {
 		if (inputSide < 0){
 			transform.Translate(Vector2.left * movementModifier);
 		}
-		if (inputUp > 0) {
+		if (inputUp > 0 && canJump == true){
 			transform.Translate(Vector2.up * verticalModifier);
 		}
+		canJump = false;
+		//print (Mathf.Exp (test));
+	}
+	void OnCollisionStay2D (Collision2D col)
+	{
+
+		if (col.gameObject.tag == "Terrain") {
+			canJump = true;
+
+		}
+
 	}
 }
