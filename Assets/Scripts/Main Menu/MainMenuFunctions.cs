e459f9b8b3c8a6bae4a3beaaadc15ff92656c538
thejereman13 <jereman13@gmail.com>
2015-08-29 14:13:22 -0400

    Main Menu Functionality Work
    Movement around the menu works with arrow keys (and wasd)  and added
    functionality to the load level button.

---
+++ Assets/Scripts/Main Menu/MainMenuFunctions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void clickEvent(){
+		Application.LoadLevel (1);
+	}
+}
