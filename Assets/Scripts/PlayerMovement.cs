42508c77787cf727dd6231df2baa3ea3c5f54bb9
Uberpangolin <jolewis@student.csdspartans.org>
2015-08-30 20:24:10 -0400

    Worked with character movement. Cleaned code up
    yeah I did that... fuck you cameron

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,10 @@ public class PlayerMovement : MonoBehaviour {
 	public bool canJump, moveLeft, moveRight;
 	public float inputSide;
 	public float BetweenJumps;
-	public int Jumpsleft = 2;
+	public int jumpsLeft = 2;
+	public int afterDJump = 0; 
+	bool Djump_Active = false;
+
 
 	Rigidbody2D rb;
 	DevOptions dev;
@@ -15,11 +18,13 @@ public class PlayerMovement : MonoBehaviour {
 	//public float test = 1.0f;
 	// Use this for initialization
 	void Start () {
+
 		rb = GetComponent<Rigidbody2D> ();
 		dev = GetComponent<DevOptions> ();
 		col = GetComponent<PolygonCollider2D>();
 		moveLeft = true;
 		moveRight = true;
+
 	}
 	
 	// Update is called once per frame
@@ -39,45 +44,50 @@ public class PlayerMovement : MonoBehaviour {
 		}
 		bool spaceHeldDown = Input.GetKeyDown (KeyCode.Space);
 		//test = test + 0.1;
+		//button held down checking stuff
 		bool inputUp = Input.GetButton("Jump");
 		bool inputDown = Input.GetButton("Croutch");
 		inputSide = Input.GetAxis ("Horizontal");
-		float velocityUp;
-		if (inputUp == true) {
-			velocityUp = 20;
-		} else {
-			velocityUp = -20;
+		if (Djump_Active) {
+			afterDJump++;
 		}
 		BetweenJumps--;
-			//Needs a movement function for accurate movement, Josh pls do this
+		//Needs a movement function for accurate movement, Josh pls do this
 		//Check for side collision
+
+		//code that checks for right movement
 		if (inputSide > 0 && moveRight){
 			transform.Translate(transform.right * movementModifier);
 			moveLeft = true;
 			moveRight = true;
 		}
+		//code that checks for left movement
 		if (inputSide < 0 && moveLeft){
 			transform.Translate(transform.right * movementModifier * -1);
 			moveRight = true;
 			moveLeft = true;
 		}
+		//First Jump code
 		if (inputUp && canJump && noclip == false && fly == false){
 			rb.AddForce(transform.up * verticalModifier * 100, ForceMode2D.Impulse);
 			moveRight = true;
 			moveLeft = true;
-			Jumpsleft--;
+			jumpsLeft--;
 			BetweenJumps = 5;
+			Djump_Active = true;
 		}
-		if (BetweenJumps <= 0 && Jumpsleft > 0 && spaceHeldDown == true && canJump == false) {
-			//rb.AddForce (new Vector2 (inputSide * 900, velocityUp * 100), ForceMode2D.Impulse);
-			rb.AddForce(transform.up * verticalModifier * 200, ForceMode2D.Impulse);
+		//Double Jump code
+		if (BetweenJumps <= 0 && jumpsLeft > 0 && spaceHeldDown == true && canJump == false) {
+			rb.AddForce((transform.up * verticalModifier * 200), ForceMode2D.Impulse);
 			BetweenJumps = 5;
-			Jumpsleft--;
+			jumpsLeft--;
+		//Rising up if noclip or fly is active
 		}
 		if (inputUp & (noclip || fly)) {
 			transform.Translate(transform.up * .5f);
 			moveRight = true;
 			moveLeft = true;
+		//Lowering down if noclip or fly is active
 		}
 		if (inputDown & (noclip || fly)) {
 			transform.Translate(transform.up * -.5f);
@@ -87,19 +97,21 @@ public class PlayerMovement : MonoBehaviour {
 		print (canJump);
 		canJump = false;
 	}
-	void OnCollisionStay2D (Collision2D col){
-		ContactPoint2D contact = col.contacts[0];
-		if(Vector2.Dot(contact.normal, Vector3.up) > 0.1)
-		{
+		//Collision detection and that stuff
+		void OnCollisionStay2D (Collision2D col){
+		ContactPoint2D contact = col.contacts[0]; 
+		if(Vector2.Dot(contact.normal, Vector2.up) > 0.1){
 			canJump = true;
-			Jumpsleft = 2;
+			jumpsLeft = 2;
+			Djump_Active = false;
+			afterDJump = 0;
 		}
-		if (Vector2.Dot (contact.normal, Vector2.right) > 0.1) {
-			moveLeft = false;
-		}
-		if (Vector3.Dot (contact.normal, Vector2.right) < 0) {
-			moveRight = false;
-		}
+		//if (Vector2.Dot (contact.normal, Vector2.right) > 0.1) {
+			//moveLeft = false;
+		//}
+		//if (Vector3.Dot (contact.normal, Vector2.right) < 0) {
+			//moveRight = false;
+		//}
 		//Code maybe to detect side collision for movement?
 		
 	}
