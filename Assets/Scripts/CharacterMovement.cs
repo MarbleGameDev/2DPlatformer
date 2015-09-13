ea312d590b2d60df489215a11cabd1d98ca8d70e
thejereman13 <jereman13@gmail.com>
2015-09-13 15:51:05 -0400

    Actually Fixed Jump this Time
    Guess What Josh, I Was Right! Velocity Is The Best Method For Movement.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -1,59 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class CharacterMovement : MonoBehaviour {
 	public AudioClip deathSound;
-	public float verticalModifier;
-	public float horizontalModifier;
-	public Vector2 movementDirection = new Vector2(0,0);
+	public int terminalVelocity;
+	public int horizontalMax;
+	private float jumpVelocity = 30;
 	Rigidbody2D rb;
 	DevOptions dev;
 	PolygonCollider2D col;
 	public bool isGrounded;
 	public bool collisionLeft;
 	public bool collisionRight;
-	public int jumpsLeft = 1;
-	// Use this for initialization
+	private float gravityScale;
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		dev = GetComponent<DevOptions> ();
 		col = GetComponent<PolygonCollider2D>();
+		gravityScale = rb.gravityScale;
 	} 
 	// Update is called once per frame
 	void Update () {
 		bool noclip = dev.noclip;
 		bool fly = dev.fly;
 		bool debug = dev.debug;
-		if (Input.GetButton ("Jump") && (isGrounded || (collisionLeft && rb.velocity.y == 0) || (collisionRight && rb.velocity.y == 0))) {
-			movementDirection.y = 3;
-			print ("jump");
+		if (fly || noclip) {
+			rb.gravityScale = 0;
+			jumpVelocity = 1;
 		} else {
-			movementDirection.y = -.05f;
+			if (rb.gravityScale == 0){
+				rb.gravityScale = gravityScale;
+			}
+			if (jumpVelocity != 30){
+				jumpVelocity = 30;
+			}
+		}
+		if (Input.GetButton ("Jump") && (isGrounded || (collisionLeft && rb.velocity.y == 0) || (collisionRight && rb.velocity.y == 0) || fly || noclip)) {
+			rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y + jumpVelocity);
+		} else {
+			if ((rb.velocity.y > terminalVelocity) && !noclip && !fly){
+				rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y - 1);
+			}
 		}
 		if (Input.GetButton ("Left") && !collisionLeft) {
-				movementDirection.x = -.5f;
-		}
-		if (Input.GetButton ("Right") && !collisionRight) {
-				movementDirection.x = .5f;
-		}
-		if (isGrounded) {
-			jumpsLeft = 1;
-		}
-		//jumping
-		if ((movementDirection.y > 0 || fly || noclip)&& jumpsLeft > 0 && movementDirection.x == 0) {
-			rb.AddForce (new Vector2 (0, movementDirection.y) * verticalModifier, ForceMode2D.Impulse);
-			print ("Enter 0");
-		}
-		else if ((Mathf.Abs (rb.velocity.x) < 30)&& jumpsLeft > 0 && rb.velocity.y >= 0) {
-			rb.AddForce (new Vector2 (movementDirection.x,0) * horizontalModifier, ForceMode2D.Impulse);
-			rb.AddForce (new Vector2 (0,movementDirection.y) * verticalModifier, ForceMode2D.Impulse);
-			print ("Enter 1");
-		} else if ((Mathf.Abs (rb.velocity.x) < 30) && rb.velocity.y < 0) {
-			rb.AddForce (new Vector2 (movementDirection.x, 0) * horizontalModifier * .5f, ForceMode2D.Impulse);
-			print ("Enter 2");
-		} else if ((Mathf.Abs (rb.velocity.x) >= 30) && rb.velocity.y >= 0) {
-			rb.AddForce (new Vector2 (movementDirection.x * .1f, movementDirection.y * 0), ForceMode2D.Impulse);
-			print ("Enter 3");
+			if (rb.velocity.x > -horizontalMax) {
+				rb.velocity = new Vector2 (rb.velocity.x - 10, rb.velocity.y);
+			}
+		} else if (Input.GetButton ("Right") && !collisionRight) {
+			if (rb.velocity.x < horizontalMax) {
+				rb.velocity = new Vector2 (rb.velocity.x + 10, rb.velocity.y);
+			}
+		} else {
+			if (Math.Floor(rb.velocity.x) != 0){
+				rb.velocity = new Vector2 (rb.velocity.x / 2, rb.velocity.y);
+			}
 		}
 		if (debug) {
 			Debug.Log (rb.velocity);
@@ -62,8 +63,6 @@ public class CharacterMovement : MonoBehaviour {
 		positionChecking ();
 
 		//Changing Stuff
-		movementDirection.x = 0;
-		movementDirection.y = 0;
 		isGrounded = false;
 		collisionLeft = false;
 		collisionRight = false;
