be7508c380167ba9e326a7256ebde90b122f00e7
thejereman13 <jereman13@gmail.com>
2015-10-24 15:48:53 -0400

    Ok, I think I fixed the menu crap I deleted

---
+++ Assets/Menus/MenuManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+	public Transform window;
+	static Transform newWindow;
+	static string currentWindowName;
+	public Transform mainMenu;
+	public Transform optionsMenu;
+	public static bool windowOpen = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void OpenWindow(string name){
+		windowOpen = true;
+		currentWindowName = name;
+		switch (name) {
+		case "mainMenu":
+			newWindow = Instantiate(mainMenu);
+			newWindow.SetParent(window, false);
+			break;
+		case "optionsMenu":
+			newWindow = Instantiate(optionsMenu);
+			newWindow.SetParent(window, false);
+			break;
+		}
+	}
+	public void CloseWindow(){
+		windowOpen = false;
+		Destroy(GameObject.Find (currentWindowName + "(Clone)"));
+	}
+}
