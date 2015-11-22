75f85f67e35ef9c5eb8f48c20b77951851dd3030
thejereman13 <jereman13@gmail.com>
2015-11-22 17:55:37 -0500

    Character Walk Animation
    It's awesome

--- Assets/Scripts/TopViewCharactermovement.cs
+++ Assets/Scripts/TopViewCharactermovement.cs
@@ -3,23 +3,32 @@ using System.Collections;
 using System;
 
 public class TopViewCharactermovement : MonoBehaviour {
-	public AudioClip deathSound;
-	public Sprite leftSprite;
-	public Sprite rightSprite;
-	public Sprite frontSprite;
-	public Sprite backSprite;
 	private float horizontalMax = 10f;
 	private float jumpVelocity = 30;
+
+
 	Rigidbody2D rb;
 	DevOptions dev;
 	PolygonCollider2D col;
-	public bool isGrounded;
+
+
 	public bool collisionLeft;
 	public bool collisionRight;
 	public bool collisionUp;
 	public bool collisionDown;
 	private float gravityScale;
-	SpriteRenderer sprite;
+
+	int direction;
+
+	private bool left;
+	private bool right;
+	private bool up;
+	private bool down;
+	private bool horizontal;
+	
+
+	Animator anim;
+
 
 	// Use this for initialization
 	void Start () {
@@ -27,59 +36,69 @@ public class TopViewCharactermovement : MonoBehaviour {
 		dev = GetComponent<DevOptions> ();
 		col = GetComponent<PolygonCollider2D>();
 		gravityScale = rb.gravityScale;
-		sprite = GetComponent<SpriteRenderer> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		bool noclip = dev.noclip;
-		bool fly = dev.fly;
-		bool debug = dev.debug;
-		if (Input.GetButton ("Left") || Input.GetButton ("Right") || Input.GetButton ("Up") || Input.GetButton ("Down")) {
-			if (Input.GetButton ("Left") && !collisionLeft) {
-				if (rb.velocity.x > -horizontalMax) {
-					rb.velocity = new Vector2 (rb.velocity.x - 2f, rb.velocity.y);
-				}
-				sprite.sprite = leftSprite;
-			} else if (Input.GetButton ("Right") && !collisionRight) {
-				if (rb.velocity.x < horizontalMax) {
-					rb.velocity = new Vector2 (rb.velocity.x + 2f, rb.velocity.y);
-				}
-				sprite.sprite = rightSprite;
-			} else {
-				if (Math.Floor (rb.velocity.x) != 0) {
-					rb.velocity = new Vector2 (rb.velocity.x / 2, rb.velocity.y);
-				}else{
-					rb.velocity = new Vector2 (0,rb.velocity.y);
-				}
-			}
-			if (Input.GetButton ("Up") && !collisionUp) {
-				if (rb.velocity.y < horizontalMax) {
-					rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y + 2f);
-				}
-				sprite.sprite = backSprite;
-			} else if (Input.GetButton ("Down") && !collisionDown) {
-				if (rb.velocity.y > -horizontalMax) {
-					rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y - 2f);
-				}
-				sprite.sprite = frontSprite;
-			} else {
-				if (Math.Floor (rb.velocity.y) != 0) {
-					rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y / 2);
-				}else{
-					rb.velocity = new Vector2 (rb.velocity.x,0);
-				}
-			}
-		}else{
-			rb.velocity = new Vector2 (0,0);
-		}
-		if (debug) {
-			Debug.Log (rb.velocity);
-		}
+	void FixedUpdate () {
+		if (!up && !down)
+		left = Input.GetButton ("Left");
+		if (!up && !down)
+		right = Input.GetButton ("Right");
+		if (!left && !right)
+		up = Input.GetButton ("Up");
+		if (!left && !right)
+		down = Input.GetButton ("Down");
 
+		if (left && !collisionLeft) {
+			direction = 1;
+			if (rb.velocity.x > -horizontalMax) {
+				rb.velocity = new Vector2 (rb.velocity.x - 2f, 0);
+			}
+			anim.Play ("Walking Left");
+		} else if (right && !collisionRight) {
+			direction = 2;
+			if (rb.velocity.x < horizontalMax) {
+				rb.velocity = new Vector2 (rb.velocity.x + 2f, 0);
+			}
+			anim.Play("Walking Right");
+		}else if (up && !collisionUp) {
+			direction = 3;
+			if (rb.velocity.y < horizontalMax) {
+				rb.velocity = new Vector2 (0, rb.velocity.y + 2f);
+			}
+			anim.Play("Walking Up");
+		} else if (down && !collisionDown) {
+			direction = 4;
+			if (rb.velocity.y > -horizontalMax) {
+				rb.velocity = new Vector2 (0, rb.velocity.y - 2f);
+			}
+			anim.Play("Walking Down");
+		} else {
+			if (Math.Floor (rb.velocity.y) != 0) {
+				rb.velocity = new Vector2 (rb.velocity.x / 2, rb.velocity.y / 2);
+			} else {
+				rb.velocity = new Vector2 (0, 0);
+			}
+			Debug.Log (direction);
+			switch (direction){
+			case 1:
+				anim.Play ("StandLeft");
+				break;
+			case 2:
+				anim.Play ("StandRight");
+				break;
+			case 3:
+				anim.Play ("StandUp");
+				break;
+			case 4:
+				anim.Play ("StandDown");
+				break;
+			}
+
+		}
 		
 		//Changing Stuff
-		isGrounded = false;
 		collisionLeft = false;
 		collisionRight = false;
 		collisionUp = false;
