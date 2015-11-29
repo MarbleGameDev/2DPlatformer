ef08f80d0ec4bd9702362806548f65097033d6eb
thejereman13 <jereman13@gmail.com>
2015-11-28 23:37:20 -0500

    Setup Lighting and Candle
    Moved everything over to dispersed lighting in order to facilitate
    dynamic lighting in scenes rather than global illumination. To test, a
    spot light was added to the candle on the table and a flicker script was
    made to simulate the candle flicker (to some extent). Will work more on
    it like the daylight cycle once the main world is created.

---
+++ Assets/Scripts/CandleFlicker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandleFlicker : MonoBehaviour {
+	Light light;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		InvokeRepeating ("Light", 0f, .25f);
+	}
+	
+	// Update is called once per frame
+	void Light () {
+		float num = Random.value;
+		float direction = Random.value;
+		if (direction <= 0.5f && light.intensity < 1.5f && light.intensity > .75f) {
+			light.intensity = Mathf.SmoothStep (light.intensity, light.intensity - (num / 2), 0.25f);
+		} else if (direction > 0.5f && light.intensity < 1.5f && light.intensity > .75f) {
+			light.intensity = Mathf.SmoothStep (light.intensity, light.intensity + (num / 2), 0.25f);
+		} else {
+			light.intensity = Mathf.SmoothStep (light.intensity, 1, 0.25f);
+		}
+	}
+}
