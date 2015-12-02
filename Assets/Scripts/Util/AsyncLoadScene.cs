f7347f6962dbd3bdadb8dbbb9608d2adbf3e733f
thejereman13 <jereman13@gmail.com>
2015-12-02 16:36:23 -0500

    Setup Beginning Loading Screen
    Created scene at the beginning of the game launch to load the main menu
    and made an Async load level script for use in other loading screens as
    well. Also updated the main menu stuff to current standards. Ignore the
    extra image, it was just for testing.

---
+++ Assets/Scripts/Util/AsyncLoadScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsyncLoadScene : MonoBehaviour {
+	public string SceneName;
+	// Use this for initialization
+	void Start () {
+		Application.LoadLevelAsync (SceneName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
