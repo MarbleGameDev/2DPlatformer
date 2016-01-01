d36f560d658b63e01ae213c48e0ac218fc9933cb
thejereman13 <jereman13@gmail.com>
2016-01-01 14:11:00 -0500

    Added System to Specify Extra Children in Dialogue
    Ability added to add "child" gameobjects to the system for remote
    callbacks or just to change the course of a path without repetition.

--- Assets/Menus/Dialogue/DialogueObject.cs
+++ Assets/Menus/Dialogue/DialogueObject.cs
@@ -13,6 +13,8 @@ public class DialogueObject : MonoBehaviour, IDialogue {
 	public string prompt;
     [Header("Not used with Options")]
     public UnityEvent endFunct;
+
+    public Transform[] extraChildren;
 	// Use this for initialization
 	void Start () {
 	
@@ -26,13 +28,17 @@ public class DialogueObject : MonoBehaviour, IDialogue {
 		return Name;
 	}
 	public void OpenDialogue(){
-		if (gameObject.transform.childCount > 1) { 	//Branching Dialogue
+		if (gameObject.transform.childCount + extraChildren.Length > 1) { 	//Branching Dialogue
 			var dialogues = new List<IDialogue> ();
 			foreach (Transform child in gameObject.transform){
 				if (child.GetComponent<IDialogue>() != null)
 					dialogues.Add(child.GetComponent<IDialogue>());
 			}
-			int i = 0;
+            foreach (Transform child in extraChildren) {
+                if (child.GetComponent<IDialogue>() != null)
+                    dialogues.Add(child.GetComponent<IDialogue>());
+            }
+            int i = 0;
             dialogue = new string[dialogues.Count];
 			foreach (IDialogue dial in dialogues){
 				dialogue[i] = dial.GetName();
@@ -51,6 +57,8 @@ public class DialogueObject : MonoBehaviour, IDialogue {
             lnDial.endFunct = endFunct;
             if (gameObject.transform.childCount > 0) {
                 lnDial.nextDialogue = gameObject.transform.GetChild(0).GetComponent<IDialogue>();
+            } else if (extraChildren[0] != null) {
+                lnDial.nextDialogue = extraChildren[0].GetComponent<IDialogue>();
             } else {
                 lnDial.nextDialogue = null;
             }
