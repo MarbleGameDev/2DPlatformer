fd49529a7361777507f83921435ae360677e3306
Uberpangolin <jolewis@student.csdspartans.org>
2016-02-10 16:27:40 -0500

    Organization. Hitboxes. Quests?!?!?!?!?
    all of those and more on this comit

---
+++ Assets/Scripts/Util/NpcLayering.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NpcLayering : MonoBehaviour {
+
+
+
+	GameObject player;
+	SpriteRenderer sprite;
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Character");
+		sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (player.transform.position.y > gameObject.transform.position.y){
+			sprite.sortingOrder = 6;
+		}else{
+			sprite.sortingOrder = 3;
+		}
+	
+	}
+}
